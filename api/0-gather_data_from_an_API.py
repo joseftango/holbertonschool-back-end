92516d3108ac46cf7beeb3656484261ca0591e58
joseftango <4279@holbertonschool.com>
2023-05-04 23:50:47 +0100

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,33 +4,24 @@ import requests
 from sys import argv
 
 
-def extract_data():
-    """function that print extracted data"""
-    empolyee_id = argv[1]
-    data_user = requests.get(f'https://jsonplaceholder.\
-typicode.com/users/{empolyee_id}').json()
-
-    data_todos = requests.get(f'https://jsonplaceholder.\
-typicode.com/users/{empolyee_id}/todos/').json()
-
-    EMPLOYEE_NAME = data_user['name']
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    str2 = ""
-    for item in range(len(data_todos)):
-        if data_todos[item]['completed'] is True:
-            NUMBER_OF_DONE_TASKS += 1
-            if item == len(data_todos) - 1:
-                str2 += "\t {}".format(data_todos[item]['title'])
-            else:
-                str2 += "\t {}\n".format(data_todos[item]['title'])
-        TOTAL_NUMBER_OF_TASKS += 1
-
-    str1 = "Employee {} is done withtasks({}/{}):"\
-        .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
-    final = str1 + '\n' + str2
-    print(final)
-
-
 if __name__ == '__main__':
-    extract_data()
+
+    id = argv[1]
+    task_title = []
+    complete = 0
+    total = 0
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
+    result = requests.get(url_user).json()
+    name = result.get('name')
+    todos = "https://jsonplaceholder.typicode.com/todos/"
+    res_task = requests.get(todos).json()
+    for i in res_task:
+        if i.get('userId') == int(id):
+            if i.get('completed') is True:
+                task_title.append(i['title'])
+                complete += 1
+            total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete, total))
+    for x in task_title:
+        print("\t {}".format(x))
