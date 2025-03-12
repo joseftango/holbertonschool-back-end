3bf27022fa3381699a35897ac56347af54b2fd8c
joseftango <4279@holbertonschool.com>
2025-03-12 01:16:05 +0100

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,17 @@
-from sys import argv
+#!/usr/bin/python3
+"""0-gather_data_from_an_API module"""
 from requests import get
+from sys import argv
+
 
 employee_id = int(argv[1])
-response_employee_info = get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
+remp_res = get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+remp_res = remp_res.json()
 response_tasks = get('https://jsonplaceholder.typicode.com/todos').json()
 
 NUMBER_OF_DONE_TASKS = 0
 TOTAL_NUMBER_OF_TASKS = 0
-EMPLOYEE_NAME = response_employee_info['name']
+EMPLOYEE_NAME = remp_res['name']
 first_line = f'Employee {EMPLOYEE_NAME} is done with tasks'
 completed_taskes_title = []
 
@@ -23,4 +27,4 @@ text = first_line + f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
 for t in completed_taskes_title:
     text += '\n\t ' + t
 
-print(text)
+print(f'{text}')
