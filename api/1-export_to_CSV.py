dd438b2f252d3b799bbf14bb2f49df6accb10b9d
joseftango <4279@holbertonschool.com>
2025-03-12 02:19:45 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,20 +5,21 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    employee_id = int(argv[1])
-    remp_res = get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    em_id = int(argv[1])
+    remp_res = get('https://jsonplaceholder.typicode.com/users/{}'
+                   .format(em_id))
     remp_res = remp_res.json()
     response_tasks = get('https://jsonplaceholder.typicode.com/todos').json()
 
     USERNAME = remp_res['name']
     USER_ID = remp_res['id']
-    FILE_NAME = f'{employee_id}.csv'
+    FILE_NAME = '{}.csv'.format(em_id)
 
     with open(FILE_NAME, 'w', encoding='UTF-8') as csv_file:
         for task in response_tasks:
-            if task['userId'] == employee_id:
+            if task['userId'] == em_id:
                 TASK_COMPLETED_STATUS = task['completed']
                 TASK_TITLE = task['title']
-                record = f'"{USER_ID}","{USERNAME}",\
-"{TASK_COMPLETED_STATUS}","{TASK_TITLE}"\n'
+                record = '"{}","{}",\
+"{}","{}"\n'.format(USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE)
                 csv_file.write(record)
