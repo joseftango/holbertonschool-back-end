289c0a98a5c8342c6512d451c8c62d374a3c0e8b
joseftango <4279@holbertonschool.com>
2025-03-13 20:12:03 +0100

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""model named 0-gather_data_from_an_API"""
+'''
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+export data to the JSON format.
+'''
 import json
 import requests
-
-
-def export_data_to_json():
-    """function that extract data to export it in json"""
-    data_users = requests.get(f'https://jsonplaceholder.\
-typicode.com/users/').json()
-
-    data_todos = requests.get(f'https://jsonplaceholder.\
-typicode.com/todos/').json()
-    data_dict = {}
-    for obj in data_users:
-        data_dict[f"{obj['id']}"] = []
-        data_todos_user = requests.get(f"https://jsonplaceholder.\
-typicode.com/user/{obj['id']}/todos/").json()
-        for i in data_todos_user:
-            todos_dict = {}
-            todos_dict['username'] = obj['username']
-            todos_dict['task'] = i['title']
-            todos_dict['completed'] = i['completed']
-            data_dict[f"{obj['id']}"].append(todos_dict)
-
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(data_dict, f)
+import sys
 
 
 if __name__ == '__main__':
-    export_data_to_json()
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    res = requests.get(url_user).json()
+    data = {}
+    values = []
+
+    for user in res:
+        id = str(user['id'])
+        username = user['username']
+        res_tasks = requests.get(
+                'https://jsonplaceholder.typicode.com/users/' +
+                id + '/todos').json()
+        for t in res_tasks:
+            values.append({'username': username, 'task': t['title'],
+                           'completed': t['completed']})
+        data[id] = values
+
+    with open('todo_all_employees.json', 'w', encoding='UTF-8') as f:
+        json.dump(data, f)
